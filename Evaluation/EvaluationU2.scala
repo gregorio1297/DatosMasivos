d58afe1eb685d0b4f7e5eb4026494f7d479b3e62
Edgar <regalado.correo@gmail.com>
2022-05-22 16:57:45 -0700

    Update EvaluationU2.scala

--- Evaluation/EvaluationU2.scala
+++ Evaluation/EvaluationU2.scala
@@ -31,22 +31,45 @@ val output = Vassembler.transform(df)
 output.show()
 
 //Transformamos la columna especial en numerica y nombrandola como label
-val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("indexSpecies").fit(df)
+val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("label").fit(df) //special note: col must always be called label to be automatic
 val indexed = labelIndexer.transform(output) 
 indexed.show()
 
 
 //Construya el modelo de clasificación y explique su arquitectura.
 
-//Dividmos nuestro data en datos de entrenamiento y datos de prueba 60% para entrenamiento y 40% para prueba 
-// seed es para iniciar en algun punto en concreto.
-val splits = indexed.randomSplit(Array(0.6, 0.4), seed = 1234L)
+// The data is splitted randomly between train set and test set, the proportion is 70% to 30% as usual.
+// The random seed is 1234L, which is a long number data type
+val splits = indexed.randomSplit(Array(0.7, 0.3), seed = 1234L)
 val train = splits(0)
 val test = splits(1)
 
-
-//Establecemos las capas de nuestra red neuronal 
-// 4 de entreda pasa por 5 y 4 capa intermedia o capa oculta y salida de 3.
+// The network architecture is specified, the input layer must have as many nodes as features there are
+// the ouput layer must have as many nodes as classification labels there are.
+// The layer in the middle are hidden for the rest of the model and have no relevant restrictions beside logical ones.
+// features: [sepal_length,sepal_width,petal_length,petal_width], clasifications: [0.0, 1.0, 2.0] 
 val layers = Array[Int](4, 5, 4, 3)
 
-//Imprima los resultados del modelo
+// create the trainer and set its parameters
+val trainer = new MultilayerPerceptronClassifier()
+ .setLayers(layers)
+ .setBlockSize(128)
+ .setSeed(1234L)
+ .setMaxIter(100)
+
+// train the model
+val model = trainer.fit(train)
+ 
+// compute accuracy on the test set || precision de calculo en el conjunto de prueba
+val result = model.transform(test)
+val predictionAndLabels = result.select("prediction", "label")
+val evaluator = new MulticlassClassificationEvaluator()
+ .setMetricName("accuracy")
+ 
+// show the accuracy 
+println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+
+//show the distribution of the data used
+println(s"train: ${train.count}, test: ${test.count()}")
+//show the table true value vs prediction
+result.select("features", "label", "prediction").show(test.count().asInstanceOf[Int])
