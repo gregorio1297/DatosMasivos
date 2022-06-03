256940036241e362145a191889ca3e55c61b82ca
gregorio1297 <gregorio.guzman17@tectijuana.edu.mx>
2022-06-03 01:22:35 -0700

    Update DT and MP files

--- ML Algorithms/Decision.scala
+++ ML Algorithms/Decision.scala
@@ -2,15 +2,30 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.DecisionTreeClassificationModel
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 
-val df = spark.read.option("header", "true").option("inferSchema","true").option("delimiter",";")csv(bank-full.csv)
+val df = spark.read.option("header", "true").option("inferSchema","true").option("delimiter",";")csv("bank-full.csv") //this dataset is separated by ; instead of ,
 
-val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(feature_data)
-// we now set the input, output and max categories with featuredata
-val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(feature_data)
+val labelIndexer = new StringIndexer().setInputCol("y").setOutputCol("indexedLabel").fit(df)
+val indexed = labelIndexer.transform(df).drop("y").withColumnRenamed("indexedLabel", "label") 
+val assembler = new VectorAssembler().setInputCols(Array("balance", "day", "duration", "previous")).setOutputCol("features")
+val features = assembler.transform(indexed)
+val filter = features.withColumnRenamed("y", "label")
+
+val finalData = filter.select("label", "features")
+
+// Index labels, adding metadata to the label column.
+// Fit on whole dataset to include all labels into the index.
+val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(finalData)
+// Automatically identify categorical features and then index them.
+val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(finalData)
+
+
+for(i <- 101 to 130) { //101 and 130 are arbitrary values and will loop 30 times
 // Split and save the data into training and test data, going with 70% and 30%
-val Array(trainingData, testData) = feature_data.randomSplit(Array(0.7, 0.3))
+val Array(trainingData, testData) = finalData.randomSplit(Array(0.7, 0.3))
 
 val dt = new DecisionTreeClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures")
 
@@ -27,15 +42,17 @@ val model = pipeline.fit(trainingData)
 val predictions = model.transform(testData)
 
 // Select example rows to display. 
-predictions.select("predictedLabel", "label", "features").show(5)
+//predictions.select("predictedLabel", "label", "features").show(5)
 
 // Select (prediction, true label)
 val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
 // Compute the test error.
 val accuracy = evaluator.evaluate(predictions)
-println(s"accuracy = ${(accuracy)}")
+println(s"seed: $i, Test set accuracy = ${(accuracy)}")
 
 // Show by stages the classification of the tree model
-val treeModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
-println(s"Learned classification tree model:\n ${treeModel.toDebugString}")
+//val treeModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
+//println(s"Learned classification tree model:\n ${treeModel.toDebugString}")
+
+}
 
--- ML Algorithms/MPerceptron.scala
+++ ML Algorithms/MPerceptron.scala
@@ -4,21 +4,24 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.feature.StringIndexer 
 
-val df = spark.read.option("header", "true").option("inferSchema","true").option("delimiter",";")csv(bank-full.csv)
 
-val Vassembler = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features")
+val df = spark.read.option("header", "true").option("inferSchema","true").option("delimiter",";")csv("bank-full.csv") //this dataset is separated by ; instead of ,
+
+val Vassembler = new VectorAssembler().setInputCols(Array("balance","day","duration","previous")).setOutputCol("features")
 val output = Vassembler.transform(df)
-output.show()
 
-val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("indexSpecies").fit(df)
-val indexed = labelIndexer.transform(output) 
+val labelIndexer = new StringIndexer().setInputCol("y").setOutputCol("indexY").fit(df)
+val indexed = labelIndexer.transform(output).withColumnRenamed("indexY", "label") 
 indexed.show()
 
-val splits = indexed.randomSplit(Array(0.7, 0.3), seed = 1234L)
+
+for(i <- 101 to 130) { //101 and 130 are arbitrary values and will loop 30 times
+
+val splits = indexed.randomSplit(Array(0.7, 0.3), seed = i)
 val train = splits(0)
 val test = splits(1)
 
-val layers = Array[Int](4, 5, 4, 3)
+val layers = Array[Int](4, 5, 4, 2)
 
 // create the trainer and set its parameters
 val trainer = new MultilayerPerceptronClassifier()
@@ -36,9 +39,10 @@ val evaluator = new MulticlassClassificationEvaluator()
  .setMetricName("accuracy")
  
 // show the accuracy 
-println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+println(s"seed: $i, Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
 
 //show the distribution of the data used
-println(s"train: ${train.count}, test: ${test.count()}")
+//println(s"train: ${train.count}, test: ${test.count()}")
 //show the table true value vs prediction
-result.select("features", "label", "prediction").show(test.count().asInstanceOf[Int])
+//result.select("features", "label", "prediction").show(test.count().asInstanceOf[Int])
+}
